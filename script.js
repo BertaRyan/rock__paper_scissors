ef448979cd02d464eb331931b244b7ff17e82e8c
Ryan Berta <ryanberta1234@gmail.com>
2022-12-23 23:37:53 -0800

    i added a scoring system and a function to determine a winner.

--- script.js
+++ script.js
@@ -1,67 +1,74 @@
+const option = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
+function compChoice() {
+    let choice = option[Math.floor(Math.random() * 3)];
+    return choice
+}
+
+function playerChoice() {
+    let player = prompt('Pick Rock, Paper, Scissors').toLowerCase();
+    return player
+}
+
+function findWinner(player, computer) {
+
+    if (player === computer) {
+        return 'Tie'
+    } else if ((player === option[0] && computer === option[2]) ||
+        (player === option[1] && computer === option[0]) ||
+        (player === option[2] && computer === option[1])) {
+            playerScore++
+        return 'Player Wins!'
+       
+    } else {
+        computerScore++
+        return 'You Lose'
+    }
+}
+
+function playRound() {
+    const playerSelection = playerChoice();
+    const computerSelection = compChoice();
+    findWinner(playerSelection, computerSelection);
 
 
-const choice = ['rock', 'paper', 'scissors']
+    console.log(playerSelection)
+    console.log(computerSelection)
+    console.log(findWinner(playerSelection, computerSelection));
+    return findWinner(playerSelection, computerSelection);
+   
+
+}
 
 
 function game() {
-    for(let i = 0; i <= 5; i++) {
-        playRound();
+
+    for (let i = 1; i <= 5; i++) {
+        playRound()
     }
 
+if(playerScore>computerScore){
+    console.log("You beat the computer! you are the best!")
+    return "You beat the computer! you are the best!"
+} else if (playerScore<computerScore){
+    console.log( "You got beat by a computer! better practice up!")
+    return "You got beat by a computer! better practice up!"
+} else {
+    console.log("You tied with a computer..basically a robot yourself.") 
+    return "You tied with a computer..basically a robot yourself."
 }
-
-
-
-function playRound() {
-    const playerChoice = playerSelection();
-    const computerChoice = computerSelection();
-    const winner = checkWinner(playerChoice, computerChoice);
-    console.log(computerChoice)
-    console.log(playerChoice)
-    console.log(winner)
-}
-
-
-
-
-function playerSelection() {
-    let playerInput = prompt("Type Rock, Paper, or Scissors").toLowerCase();
-    return playerInput;
 
 }
 
-function computerSelection() {
-    return choice[Math.floor(Math.random() * choice.length)]
-}
+// if(playerScore > computerScore){
 
-function validateUser(choice) {
-    while (choice.includes(choice));
-}
+//     return "You beat the computer!!";
 
-function checkWinner(person, computer) {
-    if (person === computer) {
-        return 'Tie';
-    } else if
-        ((person === "rock" && computer === "scissors") ||
-        (person === "paper" && computer === "rock") ||
-        (person === "scissors" && computer === "paper")) {
-        return 'You Win';
-    } else {
-        return "Computer wins";
-    }
-}
-
-function logWins(playerIn,computerIn){
-    if(player== winner){
-        return 'Player win'
-    }else {
-        return 'computer wins'
-    }
-   
-}
-
-
-game();
+// } else {
+//     return "You lost to the computer, Try again!";
+// }
 
 
 
@@ -73,6 +80,8 @@ game();
 
 
 
+
+game()
 
 
 
