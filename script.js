55e4667474ca9e1be0c4f567482c838df3854669
Ryan Berta <ryanberta1234@gmail.com>
2022-12-25 20:16:10 -0800

    reworked the console side game and finished it!

--- script.js
+++ script.js
@@ -1,88 +1,64 @@
-const option = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 
-function compChoice() {
-    let choice = option[Math.floor(Math.random() * 3)];
-    return choice
+const getComputerChoice = () => {
+    let options = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random() * 3)
+    return options[randomNumber]
 }
 
-function playerChoice() {
-    let player = prompt('Pick Rock, Paper, Scissors').toLowerCase();
-    return player
-}
-
-function findWinner(player, computer) {
-
-    if (player === computer) {
-        return 'Tie'
-    } else if ((player === option[0] && computer === option[2]) ||
-        (player === option[1] && computer === option[0]) ||
-        (player === option[2] && computer === option[1])) {
-            playerScore++
-        return 'Player Wins!'
-       
-    } else {
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+        return `Tie game you both picked ${playerSelection} `
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++
+        return 'You win! paper covers rock'
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++
+        return 'You win! Rock smashes Scissors'
+    } else if (playerSelection === 'Scissors' && computerSelection === 'paper') {
+        playerScore++
+        return 'You win! Scissors cuts Paper'
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         computerScore++
-        return 'You Lose'
+        return 'You lose, Paper covers Rock'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore++
+        return 'You lose! Scissors cuts Paper'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore++
+        return 'You lose! Rock smashes Scissors'
     }
 }
 
-function playRound() {
-    const playerSelection = playerChoice();
-    const computerSelection = compChoice();
-    findWinner(playerSelection, computerSelection);
+const game = () => {
 
-
-    console.log(playerSelection)
-    console.log(computerSelection)
-    console.log(findWinner(playerSelection, computerSelection));
-    return findWinner(playerSelection, computerSelection);
-   
-
-}
-
-
-function game() {
-
-    for (let i = 1; i <= 5; i++) {
-        playRound()
+    for (let i = 1; i < 5; i++) {
+        const playerSelection = prompt('Pick rock paper or scissors').toLowerCase()
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection)
+        console.log(playRound(playerSelection, computerSelection));
     }
+    if (playerScore === computerScore) {
+        console.log('you tied')
+        return 'You tied the computer, which pretty much makes you an adriod!'
+    } else if (playerScore > computerScore) {
+        console.log('you win')
+        return 'Congratz you beat the computer!'
+    } else if (playerScore < computerScore) {
+        console.log('you lost')
+        return 'You lost, Try again. If you dare!'
+    }
+    console.log(playerScore, computerScore)
 
-if(playerScore>computerScore){
-    console.log("You beat the computer! you are the best!")
-    return "You beat the computer! you are the best!"
-} else if (playerScore<computerScore){
-    console.log( "You got beat by a computer! better practice up!")
-    return "You got beat by a computer! better practice up!"
-} else {
-    console.log("You tied with a computer..basically a robot yourself.") 
-    return "You tied with a computer..basically a robot yourself."
+
 }
 
-}
-
-// if(playerScore > computerScore){
-
-//     return "You beat the computer!!";
-
-// } else {
-//     return "You lost to the computer, Try again!";
-// }
 
 
 
-
-
-
-
-
-
-
-
-
-game()
-
+game();
+console.log(playerScore, computerScore)
 
 
 
